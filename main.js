2df9d0071fcdc3429bff069888a8a6c98d97f678
8itoDevM <gustavomarcolino862@gmail.com>
2024-03-27 10:00:28 -0300

    I organized the code, now it is way easier to understand the code, because i created the handlers

--- main.js
+++ main.js
@@ -1,73 +1,3 @@
-// Menu
-
-document.querySelector("#open-nav-menu").addEventListener("click", function(){ //abre o menu
-    document.querySelector("header nav .wrapper").classList.add("nav-open")
-});
-
-document.querySelector("#close-nav-menu").addEventListener("click", function(){ //fecha
-    document.querySelector("header nav .wrapper").classList.remove("nav-open")
-});
-
-// Greeting Section
-
-function celsiusToFahr(temperature){
-    let fahr = (temperature * 9/5) + 32;
-    return fahr;
-}
-
-let currentHour = new Date().getHours();
-
-let greetingText;
-
-if(currentHour < 12){
-    greetingText = "Good Morning!";
-} else if (currentHour < 19){
-    greetingText = "Good Afternoon!";
-} else if (currentHour < 24){
-    greetingText = "Good Evening!";
-} else {
-    greetingText = "Welcome";
-}
-
-const weatherCondition = "sunny";
-const userLocation = "New York";
-let temperature = 15;
-let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside`;
-let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1)}°F outside`;
-
-console.log(celsiusToFahr(temperature));
-
-document.querySelector("#greeting").innerHTML = greetingText;
-document.querySelector("p#weather").innerHTML = celsiusText;
-
-document.querySelector(".weather-group").addEventListener("click", function(e){
-    //celsius
-    //fahr
-
-    if(e.target.id == "celsius"){
-        console.log("Clicked celsius");
-        document.querySelector("p#weather").innerHTML = celsiusText;
-    } else {
-        console.log("Clicked fahr");
-        document.querySelector("p#weather").innerHTML = fahrText;
-
-    }
-
-});
-
-// Date & time section
-
-setInterval(function(){
-    let localTime = new Date();
-
-    document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2, "0");
-    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2, "0");
-    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2, "0");
-}, 1000)
-
-// Images section
-// ./assets/gallery/image1.jpg
-
 const galleryImages = [
     {
         src: "./assets/gallery/image1.jpg",
@@ -83,16 +13,92 @@ const galleryImages = [
     }
 ];
 
-let mainImage = document.querySelector("#gallery > img");
-let thumbnails = document.querySelector("#gallery .thumbnails");
+// Menu
 
-mainImage.src = galleryImages[0].src;
-mainImage.alt = galleryImages[0].alt;
+function menuHandler(){
 
-//<img src="./assets/gallery/image1.jpg" alt="Thumbnail Image 1"
-//data-array-index="0" data-selected="true">
+    document.querySelector("#open-nav-menu").addEventListener("click", function(){ //abre o menu
+        document.querySelector("header nav .wrapper").classList.add("nav-open")
+    });
+    
+    document.querySelector("#close-nav-menu").addEventListener("click", function(){ //fecha
+        document.querySelector("header nav .wrapper").classList.remove("nav-open")
+    });
 
-galleryImages.forEach(function(image, index){
+}
+
+// Greeting Section
+
+function celsiusToFahr(temperature){
+    let fahr = (temperature * 9/5) + 32;
+    return fahr;
+}
+
+function greetingHandler(){
+    let currentHour = new Date().getHours();
+
+    let greetingText;
+
+    if(currentHour < 12){
+        greetingText = "Good Morning!";
+    } else if (currentHour < 19){
+        greetingText = "Good Afternoon!";
+    } else if (currentHour < 24){
+        greetingText = "Good Evening!";
+    } else {
+        greetingText = "Welcome";
+    }
+
+    const weatherCondition = "sunny";
+    const userLocation = "New York";
+    let temperature = 15;
+    let celsiusText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed(1)}°C outside`;
+    let fahrText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed(1)}°F outside`;
+
+    document.querySelector("#greeting").innerHTML = greetingText;
+    document.querySelector("p#weather").innerHTML = celsiusText;
+
+    document.querySelector(".weather-group").addEventListener("click", function(e){
+    //celsius
+    //fahr
+
+    if(e.target.id == "celsius"){
+        console.log("Clicked celsius");
+        document.querySelector("p#weather").innerHTML = celsiusText;
+    } else {
+        console.log("Clicked fahr");
+        document.querySelector("p#weather").innerHTML = fahrText;
+
+    }
+
+});
+}
+
+// Date & time section
+
+function clockHandler(){
+    setInterval(function(){
+        let localTime = new Date();
+    
+        document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2, "0");
+        document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2, "0");
+        document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2, "0");
+    }, 1000)
+}
+
+// Images section
+
+function galleryHandler(){
+    let mainImage = document.querySelector("#gallery > img");
+    let thumbnails = document.querySelector("#gallery .thumbnails");
+
+    mainImage.src = galleryImages[0].src;
+    mainImage.alt = galleryImages[0].alt;
+
+    //<img src="./assets/gallery/image1.jpg" alt="Thumbnail Image 1"
+    //data-array-index="0" data-selected="true">
+
+    galleryImages.forEach(function(image, index){
     let thumb = document.createElement("img");
     thumb.src = image.src;
     thumb.alt = image.alt;
@@ -115,3 +121,13 @@ galleryImages.forEach(function(image, index){
 
     thumbnails.appendChild(thumb);
 });
+}
+
+// Product section 
+
+// pageload
+
+menuHandler();
+greetingHandler();
+clockHandler();
+galleryHandler();
